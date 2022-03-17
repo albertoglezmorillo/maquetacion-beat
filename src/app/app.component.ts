3c4f7e27b47499d2c318237a77c3a67b50ce74e4
Alberto Morillo <morillo@azurglobal.es>
2022-03-17 13:01:06 +0100

    Prueba finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'maquetacion-beat';
+  bandera : boolean = false;
+  estilo : string = 'GRID';
+  datos : number[] = [1,2,3,4,5];
+
+  cambiaDatos(){
+    if(this.bandera){
+      this.datos = [1,2,3,4,5];
+    }
+    else{
+      this.datos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    }
+    this.bandera=!this.bandera;
+  }
+  cambiaEstilo(){
+    if(this.estilo==='FLEX'){
+      this.estilo='GRID';
+    }
+    else {
+      this.estilo='FLEX';
+    }
+  }
 }
